bdeee055e0d634ca5058527aeef0f46dc7325233
Ryan Kelley <kelleyr@duvalschools.org>
2021-10-28 12:15:44 -0400

    Update variables_practice.py

--- 7B/variables/variables_practice.py
+++ 7B/variables/variables_practice.py
@@ -1,1 +1,11 @@
 # Variable Practice, Ryan Kelley, 10/28/21 11:39am, version 0.0 
+
+# Variable Types -- Most Common 
+
+my_float = -1.23455 
+my_int = -18345708123
+my_string =  "I like turtles." 
+
+print(my_float)
+print(my_int)
+print(my_string)
