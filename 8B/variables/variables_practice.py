8f6bad413e7feb3bc20256964514cb55d27df64d
Ryan Kelley <kelleyr@duvalschools.org>
2021-10-28 14:21:17 -0400

    Update variables_practice.py

--- 8B/variables/variables_practice.py
+++ 8B/variables/variables_practice.py
@@ -1,1 +1,11 @@
 # Variables Practice, Ryan Kelley, 10/28/21 1:46pm, version 0.0
+
+# Common Data Types and Variables in Python 
+
+life_count = 3 # Integer value, all whole numbers, +/- and 0. 
+account_balance = 4232.57 # Floating Point value, +/-. 
+user_name = "Billy Bob Joe" # String, a collection of letters/numbers. 
+
+print(life_count)
+print(account_balance)
+print(user_name)
