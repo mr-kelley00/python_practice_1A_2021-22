06cd43dbf82e22852d4cda50481502d113dfe0fa
Ryan Kelley <kelleyr@duvalschools.org>
2021-10-29 09:21:43 -0400

    Version 0.1
    Created an INTEGER and FLOATING POINT variable.

--- 1A/variables/variables_practice.py
+++ 1A/variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Ryan Kelley, 10/29/21 09:05am, version 0.0
+# Variables Practice, Ryan Kelley, 10/29/21 09:20am, version 0.1
+
+# Three Main Types of Variables for this Class 
+
+health = 345 # INTEGERS, any +/- whole number AND 0. 
+account_balance = 12345.52 # FLOATING POINT, any +/- with decimal. 
+
+
