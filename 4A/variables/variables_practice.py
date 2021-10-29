59c602a5c6037b857a742d1e56a75e3f214d1b10
Ryan Kelley <kelleyr@duvalschools.org>
2021-10-29 14:39:46 -0400

    version 0.1
    Added string, int, and floating point data types.

--- 4A/variables/variables_practice.py
+++ 4A/variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Your Name Here, 10/29/21 2:22pm, version 0.0 
+# Variables Practice, Your Name Here, 10/29/21 2:37pm, version 0.1 
+
+# Three Common Types of Data
+
+lives_remaining = 13 # INTEGERS, any +/- WHOLE number and 0.
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal. 
+death_cry = "Curse you, Zagreus!  I cannot stand you!" 
+# Line 7 is a STRING -- collection of letters/numbers/other chars.
