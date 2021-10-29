48aa6a1c2659417da011be3dc7cd154dc47c739d
Ryan Kelley <kelleyr@duvalschools.org>
2021-10-29 09:40:15 -0400

    Update variables_practice.py

--- 1A/variables/variables_practice.py
+++ 1A/variables/variables_practice.py
@@ -1,8 +1,11 @@
-# Variables Practice, Ryan Kelley, 10/29/21 09:20am, version 0.1
+# Variables Practice, Ryan Kelley, 10/29/21 09:31am, version 0.2
 
 # Three Main Types of Variables for this Class 
 
 health = 345 # INTEGERS, any +/- whole number AND 0. 
 account_balance = 12345.52 # FLOATING POINT, any +/- with decimal. 
+user_name = "Thomas Edison" # STRING, letters/numbers printed on screen.
 
-
+print(health)
+print(account_balance)
+print(user_name)
